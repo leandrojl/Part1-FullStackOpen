7ea4003110fb3224f420b2dcc36522318ab1f374
leandrojl <leandroracing96@hotmail.com>
2024-01-30 19:37:38 -0300

    Building anecdote app

--- Anecdotes Exercise/src/App.jsx
+++ Anecdotes Exercise/src/App.jsx
@@ -1,33 +1,123 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const Content = (props) =>{
+
+  if(props.anecdote === undefined || props.anecdote === null){
+    
+    return(
+      <div>There is no anecdote yet! Please click the button.</div>
+    )
+
+  }else{
+    return(
+      <div>
+        <p>{props.anecdote}</p>
+        <p>This anecdote has: {props.vote} votes so far!</p>
+        <Button onClick={props.onClick} text={"Vote"}/>
+      </div>
+    )
+  }
+  
+    
+  }
+
+const Button = (props) =>{
+  return(
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+const Header = (props) =>{
+
+  return(
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [randomValue, setRandomValue] = useState(0);
+
+  const [anecdotes, setAnecdotes]= useState([
+    {
+      anecdote:'If it hurts, do it more often.',
+      vote: 0,
+    },
+    {
+      anecdote:'Adding manpower to a late software project makes it later!',
+      vote:0,
+    },
+    {
+      anecdote: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+      vote: 0,
+    },
+    {
+      anecdote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+      vote:0,
+    },
+    {
+      anecdote:'Premature optimization is the root of all evil.',
+      vote: 0,
+    },
+    {
+      anecdote:'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+      vote:0,
+    },
+    {
+      anecdote: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+      vote:0,
+    },
+    {
+      anecdote:'The only way to go fast, is to go well.',
+      vote: 0,
+    }
+  ])
+
+  const generateRandomNumber = () =>{
+    return Math.floor(Math.random()*8);
+  }
+
+  const randomAnecdote = () => {
+    const randomValue = generateRandomNumber();
+    setRandomValue(randomValue);
+    const { getAnecdote, getVotes } = searchAnecdote();
+    //FIX
+  }
+
+  const searchAnecdote = () =>{
+    const getAnecdote = anecdotes[randomValue].anecdote;
+    console.log("This is the getAnecdote:", getAnecdote)
+    const getVotes = anecdotes[randomValue].vote;
+    console.log("This is the getVotes:", getVotes)
+    
+    return{
+      getAnecdote,
+      getVotes
+    }
+      
+    
+
+  }
+
+  const voteAnecdote = () =>{
+    const updatedAnecdotes = [...anecdotes];
+
+    updatedAnecdotes[randomValue].vote += 1;
+
+    setAnecdotes(updatedAnecdotes);
+
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header name={"Random Anecdotes App"} />
+      <Button onClick={randomAnecdote} text={"Random anecdote"}/>
+      <Header name={"Anecdote: "} />
+      <Content anecdote={anecdote} vote={vote} onClick={voteAnecdote}/>
     </>
   )
 }
